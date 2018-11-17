a28ac9799f5b304511443dd0c79fb36a5cb03081
Suraj Kumar Patel <42140899+suraj8730@users.noreply.github.com>
2018-11-17 09:38:51 -0500

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,65 @@
+#include <iostream> 
+#include <cstring>
+using namespace std;
+
+class ZooAnimal  
+	{
+	private:
+		char *name;
+		int cageNumber;
+		int weightDate;
+		int weight;
+	public:
+	// prototype of create function goes here
+		void Create(char nameof[100] , int cage , int date , int weigh );//
+		void Destroy (); // destroy function
+		char* reptName ();// Returns the reptile name
+		int daysSinceLastWeighed (int today);
+		
+	};
+void ZooAnimal::Create  (char nameof[100] ,int cage,int date,int weigh)
+	{
+	name= nameof;
+	cageNumber=cage;
+	weightDate=date;
+	weight=weigh;
+	}
+// -------- member function to return the animal's name
+char* ZooAnimal::reptName ()
+	{
+	return name;
+	}
+// -------- member function to return the number of days
+// -------- since the animal was last weighed
+int ZooAnimal::daysSinceLastWeighed (int today)
+	{
+	int startday, thisday;
+	//** Edits on Nov 7, 2017
+	//thisday = today/100*30 + today - today/100*100;
+	//startday = weightDate/100*30 + weightDate - weightDate/100*100;
+	//if (thisday < startday) 
+	//thisday += 360;
+	//return (thisday-startday);
+
+	if (today < weightDate) 
+	today += 360;
+	return (today-weightDate);
+	}
+
+      
+   // -------- member function to return the animal's name
+  
+   
+   // ========== an application to use the ZooAnimal class
+int main ()
+   {
+    ZooAnimal bozo;
+    bozo.Create ("Bozo", 408, 1027, 400);
+
+    cout << "This animal's name is " << bozo.reptName() << endl;
+
+   // bozo.Destroy ();
+	
+   }
+
+
